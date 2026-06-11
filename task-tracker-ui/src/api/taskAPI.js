1cd483cfd82c5135cb2fef0afec2602c96d8f35b
Aarcha <aarchas2829@gmail.com>
2026-06-11 17:24:47 +0530

    Updated API URL

--- task-tracker-ui/src/api/taskAPI.js
+++ task-tracker-ui/src/api/taskAPI.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8000";
+const BASE_URL = "https://my-backend.up.railway.app";
 
 export const getTasks = async () => {
   return await axios.get(`${BASE_URL}/tasks`);
